3a341f0ec6e8b1f674bf56107423abd93e66cdf2
IldarSaygafarov2 <sayildar17@gmail.com>
2025-05-19 15:11:53 +0500

    added some models and configured settings

---
+++ infrastructure/database/models/category.py
@@ -0,0 +1,14 @@
+from datetime import datetime
+
+from sqlalchemy import String
+
+from .mixins.int_id_pk import IntIdPxMixin
+from .base import Base, created_at, updated_at
+
+from sqlalchemy.orm import Mapped, mapped_column
+
+
+class Category(Base, IntIdPxMixin):
+    name: Mapped[str] = mapped_column(String(50))
+    created_at: Mapped[datetime]
+    updated_at: Mapped[datetime]
---
+++ infrastructure/database/models/product.py
@@ -0,0 +1,8 @@
+from typing import Optional
+
+from .mixins.int_id_pk import IntIdPxMixin
+from .base import Base, created_at
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+
+
